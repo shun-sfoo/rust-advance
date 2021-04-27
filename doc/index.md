4ca1b16af40bee7978285b81fdd76e184e3df746
shun-sfoo <ganymede0915@gmail.com>
2021-04-27 23:31:43 +0800

    start

---
+++ doc/index.md
@@ -0,0 +1,14 @@
+# The book
+
+## 第二章 猜猜看游戏教程
+
+[知识点](知识点)
+
+使用某个外部 crate 时，cargo search 找到最新语义化版本 (Semantic Versioning)
+
+### 语义化版本 (也称为 SemVer)
+
+[语义化版本](https://semver.org/lang/zh-CN/)
+
+这是一种定义版本号的标准， `0.5.5` 实际上是 `^0.5.5` 的
+简写，它表示 『任何与 `0.5.5` 版本公有 API 相兼容的版本』。
---
+++ guessing_game/src/main.rs
@@ -0,0 +1,14 @@
+use rand::Rng;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(101);
+    println!("Please input your guess.");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {}", guess);
+}
