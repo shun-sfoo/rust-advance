611e796032ece5d389f384b0eb06ac2f8416d118
shun-sfoo <ganymede0915@gmail.com>
2021-05-03 22:49:24 +0800

    variable

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,4 +4,11 @@ fn main() {
     println!("The value of x is :{}", x);
     x = 6;
     println!("The value of x is :{}", x);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let o = x.0;
+    let t = x.1;
+    let r = x.2;
+
+    let w = x.0;
 }
